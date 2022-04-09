22ee60d2bd32cb0854aec9ee67c9e3b3db1dad3a
Herie <heriemarie@gmail.com>
2022-04-09 22:20:42 +0800

    Initial Commit

---
+++ Shared/IndexAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IndexAppApp.swift
+//  Shared
+//
+//  Created by Herie Marie A. Estaño on 4/9/22.
+//
+
+import SwiftUI
+
+@main
+struct IndexAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
